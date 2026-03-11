197bfbf38414a9f0198a19098042dabdf9cb2c0c
pedrogren <pedroaa1223@gmail.com>
2026-03-11 16:04:28 -0300

    Add files via upload

---
+++ while 2.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um numero"))
+resultado = 1
+contador = 1
+while contador<=numero:
+    resultado = contador*resultado
+    contador = contador+1
+print(resultado)
+
+    
